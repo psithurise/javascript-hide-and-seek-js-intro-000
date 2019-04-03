8c16bbb14c736c04f25535a925db4c503f88ea86
Rose Stough <psithurise@programmer.net>
2019-04-03 21:39:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var lis = document.querySelectorAll('ul#ranked-list li')
+  var lis = document.querySelectorAll('.ranked-list li')
 
   for(let i = 0; i<lis.length; i++) {
-    lis[i].innerHTML= parseInt(lis[i].innerHTML)+n
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
   }
 }
 
