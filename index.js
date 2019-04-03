cf62cb68b8eff11e73fc2b0b43b9c7a39f66b454
Rose Stough <psithurise@programmer.net>
2019-04-03 21:31:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector('selector')
+}
 
+function nestedTarget() {
+  return document.querySelector('div#nested div.target')
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById('app').querySelectorAll('div#ranked-list li')
+
+  for(let i = 0; i<lis.length; i++) {
+    let temp = parseInt(lis[i]) + n
+    lis[i] = temp
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
